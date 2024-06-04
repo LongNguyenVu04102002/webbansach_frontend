e04e332908a0e6741778f3ad682835146913b6c8
Nguyen Vu Long <longnv04102002@gmail.com>
2024-06-04 21:51:38 +0700

    them cac page nho

---
+++ src/layouts/homepage/homepage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./component/Banner";
+import Carousel from "./component/Carousel";
+
+
+function HomePage(){
+    return(
+        <div>
+            <Banner />
+            <Carousel />
+        </div>
+    );
+}
+export default HomePage;
